5e84fbe6df55b2ea30baa327561bf586adee1cf8
kentshih <kentshih@etusolution.com>
2014-10-19 09:43:34 +0800

    no message

---
+++ src/faceapp/facebookConnect.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014年10月18日
+
+@author: kent
+'''
+import facebook
+import urllib2
+import cv2
+import numpy
+from django.core.files import File
+from django.core.files.base import ContentFile
+
+facebook_app_id = '258318471185'
+facebook_app_secret = 'da26ac5b7675c75cc2b9a25d87b6b8af'
+
+
+class current_user:
+    profile_url = None
+    id = None
+    name = None
+    photos = None
+    access_token = None
+    
+
+def getFBContent(cookies):
+#             cookies = dict((n, self.cookies[n].value) for n in self.cookies.keys())
+    
+    user = current_user()
+    cookie = facebook.get_user_from_cookie(
+        cookies, facebook_app_id, facebook_app_secret)
+    
+    if cookie == None :
+        return 
+    
+    graph = facebook.GraphAPI(cookie["access_token"])
+    profile = graph.get_object("me")
+    user.name = profile['name']
+    user.profile_url = profile['link']
+    user.id = profile['id']
+    user.access_token = cookie['access_token']
+    
+    photos = graph.get_object('me?fields=photos')
+    user.photos = []
+        
+        
+#         photos['photos']['data'][0]['images'][0]['source']
+        
+    for item in photos['photos']['data'] :
+        for item3 in item['images'] :
+            user.photos.append(item3['source'])
+            
+    return user
+            
+            
+def download_photo(img_url):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36')]
+    response = opener.open(img_url)
+#     htmlData = response.read()
+    return response
+def faceDetect(img):
+    cascPath = '/Users/kent/Documents/workspace/FaceDetect/src/haarcascade_frontalface_default.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    file_bytes = numpy.asarray(bytearray(img.read()), dtype=numpy.uint8)
+    img_data_ndarray = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
+    
+    if img_data_ndarray.shape[1] > 600 :
+        fx = float(600) / img_data_ndarray.shape[1]
+        fy = fx
+        img_data_ndarray = cv2.resize(img_data_ndarray, (0, 0), fx=fx , fy=fy) 
+    
+    gray = cv2.cvtColor(img_data_ndarray, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(img_data_ndarray.shape[1] / 100, img_data_ndarray.shape[1] / 100),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+    
+    print "Found {0} faces!".format(len(faces))
+    if len(faces) == 0 :
+        return None
+    
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img_data_ndarray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    
+    img_data_ndarray = cv2.imencode('.jpg', img_data_ndarray)[1].tostring()
+    file = ContentFile(img_data_ndarray)
+    file.name = 'aa.jpg'
+    return file
--- src/faceapp/views.py
+++ src/faceapp/views.py
@@ -5,95 +5,60 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from faceapp.models import Document
 from faceapp.form import DocumentForm
-import cv2
-import sys
-import numpy
-from faceapp.faceDetectHandler import faceDetectHandler
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from django.core.files import File
+
+from faceapp import facebookConnect
 # Create your views here.
 # Create your views here.
 
 def home(request):
-#     excuses = [
-#         "It was working in my head",
-#         "I thought I fixed that",
-#         "Actually, that is a feature",
-#         "It works on my machine",
-#     ]
-
-#     excuse = random.choice(SayHello.objects.all())
-    return render(request, "index.html", {'excuse': 333,'quantity':22 , 'price':33 })
-
-def faceDetect(img):
-    cascPath = '/Users/kent/Documents/workspace/FaceDetect/src/haarcascade_frontalface_default.xml'
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    # Read the image
-    
-#     for d in img.read():
-#         print str(d)
-    
-    file_bytes = numpy.asarray(bytearray(img.read()), dtype=numpy.uint8)
-    img_data_ndarray = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
-    
-    if img_data_ndarray.shape[1] > 600 :
-        fx = float(600)/img_data_ndarray.shape[1]
-        fy = fx
-        img_data_ndarray = cv2.resize(img_data_ndarray, (0,0), fx=fx , fy=fy ) 
-    
-    
-#     with open('/test.jpg', 'rb') as img_stream:
-#         file_bytes = numpy.asarray(bytearray(img_stream.read()), dtype=numpy.uint8)
-#         img_data_ndarray = cv2.imdecode(file_bytes, cv2.CV_LOAD_IMAGE_UNCHANGED)
-    #     img_data_cvmat = cv.fromarray(img_data_ndarray) #  convert to old cvmat if needed
-    gray = cv2.cvtColor(img_data_ndarray, cv2.COLOR_BGR2GRAY)
-    # Detect faces in the image
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(img_data_ndarray.shape[1]/100, img_data_ndarray.shape[1]/100),
-        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-    
-    print "Found {0} faces!".format(len(faces))
-    
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img_data_ndarray, (x, y), (x+w, y+h), (0, 255, 0), 2)
-     
-#     cv2.imshow("Faces found", img_data_ndarray)
-#     cv2.waitKey(0)
-    cv2.imwrite("/Users/kent/Documents/workspace/FaceWeb/media/documents/2014/10/12/image.jpg" , img_data_ndarray)
-    return File(open("/Users/kent/Documents/workspace/FaceWeb/media/documents/2014/10/12/image.jpg"))
+    return render(request, "index.html", {'excuse': 333, 'quantity':22 , 'price':33 })
 
 
 
 
 def list(request):
-    # Handle file upload
+    
+    
+    current_user = facebookConnect.getFBContent(request.COOKIES)
+#     if current_user != None :
+#         for photo in current_user.photos :
+#             dimag = facebookConnect.download_photo(photo)
+#             facesresult = facebookConnect.faceDetect(dimag)
+#             if facesresult != None :
+#                 newdoc = Document(docfile=facesresult)
+#                 newdoc.save()
 
+    
+    
+    # Handle file upload
+    
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         
         if form.is_valid():
-#             newdoc = Document(docfile = request.FILES['docfile'])
-            newdoc = Document(docfile= faceDetect(request.FILES['docfile']))
+            newdoc = Document(docfile=facebookConnect.faceDetect(request.FILES['docfile']))
             newdoc.save()
             
-#             faceDetect()
             # Redirect to the document list after POST
             return HttpResponseRedirect(reverse('faceapp.views.list'))
     else:
-        form = DocumentForm() # A empty, unbound form
+        form = DocumentForm()  # A empty, unbound form
 
     # Load documents for the list page
-    documents = Document.objects.all()
+    documents = [] 
+    
+    for item in Document.objects.all() :
+        if item.docfile != '' :
+            documents.append(item)
 
     # Render list page with the documents and the form
     return render_to_response(
         'list2.html',
-        {'documents': documents, 'form': form},
+        {'documents': documents,
+         'form': form ,
+         'current_user' :current_user ,
+         'facebook_app_id' : facebookConnect.facebook_app_id
+         },
         context_instance=RequestContext(request)
     )
     
@@ -108,11 +73,11 @@ def demo_piechart(request):
     cur = conn.cursor()
     cur.execute('select cat , count(pid) from abc_item group by cat limit 10')
     
-    print cur.description # prints the result set's schema
+    print cur.description  # prints the result set's schema
     alldata = cur.fetchall()
     
     xdata = []
-    ydata =[]
+    ydata = []
     for item in alldata :
         xdata.append(item[0])
         ydata.append(item[1])
